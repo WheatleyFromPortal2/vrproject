0cbbde167d11a89eab8a7a4f1a5fb34b8846425f
Hunter Dorrill <hunterdorrill@icloud.com>
2024-05-07 20:47:05 -0500

    update README and small changes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+[![Netlify Status](https://api.netlify.com/api/v1/badges/bdc78f10-7a48-4da7-a3a6-0e8e91c08386/deploy-status)](https://app.netlify.com/sites/fabulous-dragon-ab10d8/deploys)
 # vrproject
--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 let scene = document.querySelector('a-scene');
+let mySphere = document.createElement('a-sphere');
 function start() {
- scene = $('a-scene')[0];
- console.log("Starting Scene");
+  scene = $('a-scene')[0];
+  console.log("Starting Scene");
+  addSphere();
 }
 AFRAME.registerComponent("start-game", {
   init: start
 })
+function addSphere() {
+  scene.appendChild(mySphere);
+  console.log(`appended: ${mySphere}`)
+}
